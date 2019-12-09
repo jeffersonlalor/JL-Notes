c6f63565b8777d7fe7cef1dd2c2bbff9debdb2f4
Jefferson Oliveira <jefferson.lalor@icomp.ufam.edu.br>
2019-12-09 13:35:47 -0400

    XCode project created

---
+++ JL-Notes/JL-Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JL-Notes
+//
+//  Created by Jefferson de Oliveira Lalor on 09/12/19.
+//  Copyright © 2019 Lalor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
